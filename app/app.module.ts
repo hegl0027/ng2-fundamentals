ad746a57745372e1f9725581d976e27abef7b941
Hegland <Hegland.Matthew@mayo.edu>
2018-01-03 15:00:56 -0600

    Communicating Between Angular Components

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { EventsAppComponent } from './events-app.component'
+import { EventsListComponent } from './events/events-list.component'
+import { EventsThumbnailComponent } from './events/events-thumbnail.component'
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule ({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent],
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventsThumbnailComponent,
+        NavBarComponent
+    ],
     bootstrap: [EventsAppComponent]
 })
 
